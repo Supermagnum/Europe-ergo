d8382390b97401f50faf2aa96007f66b4490f6f7
Supermagnum <krbjhvee@online.no>
2022-12-09 05:15:00 +0100

    Update config.h
    Added license header

--- Europe_ergo_firmware/europe_ergo/keymaps/vial/config.h
+++ Europe_ergo_firmware/europe_ergo/keymaps/vial/config.h
@@ -1,6 +1,5 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
+// Copyright 2022 Supermagnum (@Supermagnum)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #define VIAL_KEYBOARD_UID {0xD4, 0x55, 0x06, 0x60, 0x67, 0xAF, 0x22, 0x72}
 
