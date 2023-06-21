920b0f5243b167d9ea361e58c7c5d1e7193b3448
Supermagnum <krbjhvee@online.no>
2023-06-21 17:15:35 +0200

    Update keymap.c
    changed values to match keymap values from here:
    https://github.com/qmk/qmk_firmware/blob/master/quantum/keymap_extras/keymap_norwegian.h

--- Europe_ergo_firmware/europe_ergo/keymaps/default/keymap.c
+++ Europe_ergo_firmware/europe_ergo/keymaps/default/keymap.c
@@ -33,11 +33,11 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x9(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       /**/                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUM,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,               /**/           KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_SCRL, KC_PAUS, KC_PSCR,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_INS,             /**/           KC_HOME, KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,                        /**/                    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,             /**/           KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_PGUP, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE,   TG(_NORWEGIAN),                                         /**/                 TG(_NORWEGIAN),   KC_PGDN, KC_RGUI, FN,      KC_RCTL,
+        KC_GRV,  NO_1,    NO_2,    NO_3,    NO_4,    NO_5,    NO_6,    NO_7,               /**/           NO_8,    NO_9,    NO_0,    NO_PLUS, NO_BSLS,  KC_SCRL, KC_PAUS, KC_PSCR,
+        KC_TAB,  NO_Q,    NO_W,    NO_E,    NO_R,    NO_T,    NO_Y,    KC_INS,             /**/           KC_HOME, NO_U,    NO_I,    NO_O,    NO_P,    NO_ARNG, NO_DIAE, KC_BSPC,
+        KC_CAPS, NO_A,    NO_S,    NO_D,    NO_F,    NO_G,    NO_H,                        /**/                    NO_J,    NO_K,    NO_L,    NO_OSTR, NO_AE , NO_QUOT, KC_ENT,
+        KC_LSFT, KC_NUBS, NO_Z,    NO_X,    NO_C,    NO_V,    NO_B,    KC_DEL,             /**/           KC_END,  NO_N,    NO_M,    NO_COMM, NO_DOT,  NO_MINS, KC_PGUP, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE,   TG(_NORWEGIAN),                             /**/                             TG(_NORWEGIAN),   KC_PGDN, KC_RGUI, FN,      KC_RCTL,
                                                                        KC_NO,              /**/           KC_UP,
                                                               KC_NO,   KC_NO,   KC_NO,     /**/  KC_LEFT, KC_DOWN, KC_RIGHT
     ),
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       /**/                    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUM,
         NO_PIPE, NO_1,    NO_2,    NO_3,    NO_4,    NO_5,    NO_6,    NO_7,               /**/           NO_8,    NO_9,    NO_0,    NO_PLUS, NO_BSLS, KC_SCRL, KC_PAUS, KC_PSCR,
         KC_TAB,  NO_Q,    NO_W,    NO_E,    NO_R,    NO_T,    NO_Y,    KC_INS,             /**/           KC_HOME, NO_U,    NO_I,    NO_O,    NO_P,    NO_ARNG, KC_RBRC, KC_BSPC,
-        KC_CAPS, NO_A,    NO_S,    NO_D,    NO_D,    NO_G,    NO_H,                        /**/                    KC_J,    KC_K,    KC_L,    NO_OSTR, NO_AE,   NO_QUOT, KC_ENT,
+        KC_CAPS, NO_A,    NO_S,    NO_D,    NO_D,    NO_G,    NO_H,                        /**/                    NO_J,    NO_K,    NO_L,    NO_OSTR, NO_AE,   NO_QUOT, KC_ENT,
         KC_LSFT, NO_LABK, NO_Z,    NO_X,    NO_C,    NO_V,    NO_B,    KC_DEL,             /**/           KC_END,  NO_N,    NO_M,    NO_COMM, NO_DOT,  NO_MINS, KC_PGUP, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE,   KC_TRANSPARENT,                                         /**/                    KC_TRANSPARENT,   KC_RALT, KC_RGUI, KC_PGDN, KC_RCTL,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE,   KC_TRANSPARENT,                             /**/                              KC_TRANSPARENT,   KC_RALT, KC_RGUI, KC_PGDN, KC_RCTL,
                                                                        KC_NO,              /**/           KC_UP,
                                                               KC_NO,   KC_NO,   KC_NO,     /**/  KC_LEFT, KC_DOWN, KC_RIGHT
     )
