816f75d34b5bb80431f1783745e191184d47aaeb
Supermagnum <krbjhvee@online.no>
2023-06-21 04:17:48 +0200

    Update keymap.c
    Added debugging to check if all keys register correctly when pressed.

--- Europe_ergo_firmware/europe_ergo/keymaps/default/keymap.c
+++ Europe_ergo_firmware/europe_ergo/keymaps/default/keymap.c
@@ -12,6 +12,15 @@ void keyboard_post_init_user(void) {
   setPinOutput(LED2_PIN);
 } 
 
+// added for debug purposes of the Pcb's:
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+// If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
+  return true;
+}
+
 enum custom_layers {
     _QWERTY,
     _NORWEGIAN,  //DVORAK
